81f6d1dd24b42acb79ca1b48de20d52e46dea3ec
Gerardo Daniel Tolentino <gerardzt94@gmail.com>
2020-10-16 22:00:13 -0500

    001Camaracomponente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import {WebcamModule} from 'ngx-webcam';
+import { CamaraComponent } from './camara/camara.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CamaraComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    WebcamModule
   ],
   providers: [],
   bootstrap: [AppComponent]
